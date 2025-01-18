fb396a9b604e7b2afa5fa7fdb9d559f13b3a89aa
Laurkan Kushu <laurkankushu@Mini-Laurkan.netis.cc>
2025-01-18 17:09:22 +0300

    добавил тесты для сервиса тегов

---
+++ src/test/java/ru/yandex/practicum/blog/service/TagServiceTest.java
@@ -0,0 +1,72 @@
+package ru.yandex.practicum.blog.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import ru.yandex.practicum.blog.model.Tag;
+import ru.yandex.practicum.blog.repository.TagRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@SpringJUnitConfig(classes = {TagServiceTest.TestConfig.class, TagServiceImpl.class})
+public class TagServiceTest {
+    @Autowired
+    private TagService tagService;
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @BeforeEach
+    void setUp() {
+        reset(tagRepository);
+    }
+
+    @Test
+    void addTagsToPost_shouldAddNewTagsToPost() {
+        List<String> tags = List.of("tag - 1", "tag - 2", "tag - 3");
+        Mockito.when(tagRepository.findTagByTitle(Mockito.any(String.class)))
+                .thenReturn(Optional.empty());
+        tagService.addTagsToPost(1L, tags);
+        verify(tagRepository, times(tags.size())).save(Mockito.any(Tag.class));
+        verify(tagRepository, times(tags.size())).linkTagToPost(Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    void addTagsToPost_shouldAddExistingTagsToPost() {
+        List<String> tags = List.of("tag - 4");
+        Mockito.when(tagRepository.findTagByTitle(Mockito.any(String.class)))
+                .thenReturn(Optional.of(new Tag(1L, "tag - 4")));
+
+        tagService.addTagsToPost(1L, tags);
+
+        verify(tagRepository, times(0)).save(Mockito.any(Tag.class));
+        verify(tagRepository, times(tags.size())).linkTagToPost(Mockito.any(), Mockito.any());
+    }
+
+    @Test
+    void unlinkAllTagsFromPost_shouldUnlinkAllTags() {
+        tagService.unlinkAllTagsFromPost(1L);
+
+        verify(tagRepository, times(1)).unlinkAllTagsFromPost(1L);
+    }
+
+    @Configuration
+    static class TestConfig {
+        @Bean
+        public TagService tagService(TagRepository tagRepository) {
+            return new TagServiceImpl(tagRepository);
+        }
+
+        @Bean
+        public TagRepository tagRepository() {
+            return mock(TagRepository.class);
+        }
+    }
+}
